fce529c62367c7dbd8882797fd2a8df413190a65
Trevor Vardeman <trevor.vardeman@rackspace.com>
2011-08-30 15:53:25 -0500

    Working with links/pagination.  Working out logic for the next/prev links.

--- api/common-api/src/main/java/org/openstack/atlas/api/helpers/LinkageUriBuilder.java
+++ api/common-api/src/main/java/org/openstack/atlas/api/helpers/LinkageUriBuilder.java
@@ -1,16 +1,12 @@
 package org.openstack.atlas.api.helpers;
 
 import org.openstack.atlas.docs.loadbalancers.api.v1.Link;
-import org.openstack.atlas.docs.loadbalancers.api.v1.LoadBalancer;
 
 import javax.ws.rs.core.UriBuilder;
 import javax.ws.rs.core.UriInfo;
-import java.net.URI;
 import java.rmi.UnexpectedException;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
-import java.util.Set;
 
 public class LinkageUriBuilder {
     private final static String NEXT = "next";
@@ -31,20 +27,38 @@ public class LinkageUriBuilder {
 
         List<Link> links = new ArrayList<Link>();
         if (idList.size() > 0) {
-            UriBuilder uriBuilderNext = buildUri(uriInfo, (limit), (idList.get(idList.size() - 1)));
-            UriBuilder uriBuilderPrevious = buildUri(uriInfo, (limit), (idList.get(0)));
+
+            Boolean prev = true;
+            Boolean next = true;
+            if (marker.equals(0)) {
+                marker = idList.get(0);
+            }
+
+            if (marker.equals(idList.get(0))) {
+                prev = false;
+            }
+            
+            if (idList.size() < limit) {
+                next = false;
+            }
+
+            if (next) {
+                UriBuilder uriBuilderNext = buildUri(uriInfo, (limit), null/*replace with correct marker*/);
+                Link nextLink = new Link();
+                nextLink.setHref(uriBuilderNext.build().toString());
+                nextLink.setRel(NEXT);
+                links.add(nextLink);
+            }
+
+            if (prev) {
+                UriBuilder uriBuilderPrevious = buildUri(uriInfo, (limit), (idList.indexOf(marker) - limit));
+                Link previousLink = new Link();
+                previousLink.setHref(uriBuilderPrevious.build().toString());
+                previousLink.setRel(PREVIOUS);
+                links.add(previousLink);
+            }
+
             UriBuilder uriBuilderself = buildUri(uriInfo, (limit), (marker));
-
-            Link nextLink = new Link();
-            nextLink.setHref(uriBuilderNext.build().toString());
-            nextLink.setRel(NEXT);
-            links.add(nextLink);
-
-            Link previousLink = new Link();
-            previousLink.setHref(uriBuilderPrevious.build().toString());
-            previousLink.setRel(PREVIOUS);
-            links.add(previousLink);
-
             Link selfLink = new Link();
             selfLink.setHref(uriBuilderself.build().toString());
             selfLink.setRel(SELF);
